676cf9a425caca59e0f49b96c2739429359c0757
BhavyaKS2779 <51109082+BhavyaKS2779@users.noreply.github.com>
2019-06-01 16:27:53 +0530

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -1,6 +1,7 @@
 n=int(input("enter the number"))
 factorial=1
-for i in range(1,n+1):
-    factorial=factorial*i
+if n>0:
+    for i in range(1,n+1):
+        factorial=factorial*i
 print(factorial)
     
