eb63ab4515b09d3d43d2f32806583162a1510830
BhavyaKS2779 <51109082+BhavyaKS2779@users.noreply.github.com>
2019-06-02 14:39:10 +0530

    Update prime.py

--- prime.py
+++ prime.py
@@ -1,8 +1,9 @@
 n=int(input())
-for i in range(2,n):
-    if(n%i==0):
+if (n>1):
+    for i in range(2,n):
+        if(n%i==0):
         print("No")
         break
-else:
-    print("Yes")
+    else:
+        print("Yes")
     
