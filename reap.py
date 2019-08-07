ac431a8053c4ef7645742e92998800d583ac4649
BhavyaKS2779 <51109082+BhavyaKS2779@users.noreply.github.com>
2019-08-07 22:00:41 +0530

    Update reap.py

--- reap.py
+++ reap.py
@@ -1,10 +1,14 @@
 n=int(input())
 a=list(map(int,input().split()))
 l=[]
-for i in a:
-    if a.count(i)>1:
-        l.append(i)
-print(*set(sorted(l)))
+if(sorted(set(a))==sorted(a)):
+    print("unique")
+else:
+    for i in a:
+        if a.count(i)>1:
+            l.append(i)
+    print(*set(sorted(l)))
+    
 
     
     
